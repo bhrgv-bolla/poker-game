c151b359e8b090c79bd439e7e8ff3f59524786b6
bhargav bolla <hire.bhargav@gmail.com>
2018-09-20 17:57:18 -0700

    Transform code

--- src/main/java/org/bbolla/pokergame/fivecard/etl/BufferedETLProcessor.java
+++ src/main/java/org/bbolla/pokergame/fivecard/etl/BufferedETLProcessor.java
@@ -1,6 +1,9 @@
 package org.bbolla.pokergame.fivecard.etl;
 
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
+import org.bbolla.pokergame.fivecard.Card;
+import org.bbolla.pokergame.fivecard.Deck;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -8,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -22,13 +26,29 @@ public class BufferedETLProcessor {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    private final int bufferSize = 1000000 * 100 * 2; // 1 million lines * 100 characters each * 2 bytes per character ( depends on charset )
+    private final int bufferSize = 1000000 * 10; // 1 million lines * 100 characters each * 2 bytes per character ( depends on charset )
 
     private static AtomicBoolean inProcessing = new AtomicBoolean(false);
+
+    private static final LinkedHashMap<String, Integer> cardMap;
+    private static final String insertSql;
+
+    static {
+        cardMap = Maps.newLinkedHashMap();
+        int i=0;
+        for(Card card : Deck.getCards()) {
+            cardMap.put(card.toString(), i++);
+        }
+        String[] questionMarks = new String[54];
+        Arrays.fill(questionMarks, '?');
+        insertSql = "insert into poker.combination_matrix("+ String.join(",", cardMap.keySet()) +
+                    ", hand_ranking, sub_rank) values ("+String.join(",", questionMarks)+")";
+    }
 
     public CompletableFuture execute() {
         if(inProcessing.get()) throw new RuntimeException("Busy; Try Later");
         inProcessing.set(true);
+        jdbcTemplate.execute("TRUNCATE poker.combination_matrix"); //reset state; TODO make backup before resetting
         return CompletableFuture.runAsync(
                 () -> {
                     try {
@@ -58,13 +78,26 @@ public class BufferedETLProcessor {
     }
 
     private List<Object[]> transform(List<String> records){
-        return records.stream().map(r -> r.split(",")).collect(Collectors.toList());
+        List<String[]> recs =  records.stream().map(r -> r.split(",")).collect(Collectors.toList());
+        return recs.stream().map(
+                rec -> {
+                    Object[] args = new Object[54];
+                    String[] keys = Arrays.copyOf(rec, 7);
+                    args[52] = rec[7];
+                    args[53] = rec[8];
+                    for(String key: keys) {
+                        int idx = cardMap.get(key);
+                        args[idx] = 1;
+                    }
+                    return args;
+                }
+        ).collect(Collectors.toList());
     }
 
     private void load(List<Object[]> records) throws Exception {
-        final String sql = "insert into poker.combination_matrix(?, ?, ?, ?, ?, ?, ?, hand_ranking, sub_rank) values (1,1,1,1,1,1,1,?,?)";
-        int[] updateCount = jdbcTemplate.batchUpdate(sql, records);
+        int[] updateCount = jdbcTemplate.batchUpdate(insertSql, records);
         int sum = Arrays.stream(updateCount).sum();
         log.info("Updated {} records", sum);
     }
+
 }
--- src/main/java/org/bbolla/pokergame/fivecard/etl/FileUtils.java
+++ src/main/java/org/bbolla/pokergame/fivecard/etl/FileUtils.java
@@ -1,8 +1,11 @@
 package org.bbolla.pokergame.fivecard.etl;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
+@Slf4j
 public class FileUtils {
 
     /**
@@ -13,15 +16,18 @@ public class FileUtils {
     public static String[] readLines(RandomAccessFile file, int maxDataSizeInBytes) throws IOException {
         long currentOffset = file.getFilePointer();
         long totalBytes = file.length();
+        log.info("Current {}, total {}", currentOffset, totalBytes);
         if(currentOffset == totalBytes) return null;
         else {
             long remaining = totalBytes - currentOffset;
             int bytesToRead = maxDataSizeInBytes > remaining ? (int) remaining : maxDataSizeInBytes;
+            log.info("Reading {} bytes", bytesToRead);
             byte[] buffer = new byte[bytesToRead];
             file.readFully(buffer, 0, bytesToRead);
             String lastLine = file.readLine();
             String allText = new String(buffer);
-            allText = allText + lastLine;
+            if (lastLine != null) allText = allText + lastLine;
+            log.info("read {} current {}, total {}", file.getFilePointer() - currentOffset, file.getFilePointer(), totalBytes);
             return allText.split(System.lineSeparator());
         }
     }
